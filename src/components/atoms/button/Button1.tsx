9ae9b257d9f7667bf878b4c215983bcd8202a521
hiromi <hiromi.ogawa.1223@gmail.com>
2023-03-27 09:39:47 +0900

    add: Button1.tsx

--- src/components/atoms/button/Button1.tsx
+++ src/components/atoms/button/Button1.tsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
 import Link from 'next/link'
 
-const Button1 = ({}) => {
+export type PropTypes = {
+  href: string
+  children: string
+}
+
+const Button1 = ({ href = '', children = '' }) => {
   return (
     <StyledButton1>
       <Link href={href}>
@@ -11,4 +16,26 @@ const Button1 = ({}) => {
   )
 }
 
-const StyledButton1 = styled.button``
+export default Button1
+
+const StyledButton1 = styled.div`
+  font-family: 'Raleway';
+  border: solid 1px #025f80;
+  border-radius: 24px;
+  width: 170px;
+  line-height: 1;
+  overflow: hidden;
+
+  a {
+    display: block;
+    color: #025f80;
+    padding: 11px 18.5px;
+    background-color: #fff;
+  }
+
+  span {
+    display: block;
+    position: relative;
+    text-align: center;
+  }
+`
