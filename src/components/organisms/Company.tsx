89400e2b25736785153768cbf8e4f28520c15e05
hiromi <hiromi.ogawa.1223@gmail.com>
2023-04-09 01:01:15 +0900

    add: Company.tsx

--- src/components/organisms/Company.tsx
+++ src/components/organisms/Company.tsx
@@ -1,5 +1,46 @@
-const Company = ({...props}) => {
+import { useRef, useState } from 'react'
+import useScrollTrigger from '@/hooks/useScrollTrigger'
+import styled from 'styled-components'
+import img1 from '@/images/index/Business_ph01.png'
+import Contents from '../common/Contents'
+import Heading2 from '@/components/atoms/heading/Heading2'
+import FadeInAnimation from '@/components/animation/FadeInAnimation'
+import Col2TextImg from '@/components/molecules/Col2TextImg'
 
+const Company = ({ ...props }) => {
+  const elementsRef = useRef(null)
+  const inView = useScrollTrigger(elementsRef)
+  return (
+    <StyledCompany ref={elementsRef} {...props}>
+      <Contents>
+        <Heading2>Company</Heading2>
+        <FadeInAnimation trigger={inView}>
+          <Col2TextImg
+            textButtonArea={{
+              heading: {
+                children: 'Profile'
+              },
+              text: {
+                children: 'ダミーテキストダミーテキストダミーテキスト'
+              },
+              button: {
+                href: '',
+                children: 'View More'
+              }
+            }}
+            image={{
+              src: img1,
+              alt: 'text'
+            }}
+            maskColor="#FFF"
+            rowReverse
+          />
+        </FadeInAnimation>
+      </Contents>
+    </StyledCompany>
+  )
 }
 
-export default
+export default Company
+
+const StyledCompany = styled.section``
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import FirstView from '@/components/molecules/FirstView'
 import Vision from '@/components/organisms/Vision'
 import Business from '@/components/organisms/Business'
 import Record from '@/components/organisms/Record'
+import Company from '@/components/organisms/Company'
 
 // タイプ
 import type { Records } from '@/types/data'
@@ -46,33 +47,7 @@ const Home = ({ records }: PropTypes) => {
       <Vision />
       <Business />
       <StyledRecord slideCards={slideCards} />
-      <StyledCompany ref={elementsRef}>
-        <Contents>
-          <Heading2>Company</Heading2>
-          <FadeInAnimation trigger={inView}>
-            <Col2TextImg
-              textButtonArea={{
-                heading: {
-                  children: 'Profile'
-                },
-                text: {
-                  children: 'ダミーテキストダミーテキストダミーテキスト'
-                },
-                button: {
-                  href: '',
-                  children: 'View More'
-                }
-              }}
-              image={{
-                src: img1,
-                alt: 'text'
-              }}
-              maskColor="#FFF"
-              rowReverse
-            />
-          </FadeInAnimation>
-        </Contents>
-      </StyledCompany>
+      <StyledCompany />
     </Layout>
   )
 }
@@ -83,7 +58,9 @@ const StyledRecord = styled(Record)`
   margin-top: 148px;
 `
 
-const StyledCompany = styled.section``
+const StyledCompany = styled(Company)`
+  margin-top: 128px;
+`
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
